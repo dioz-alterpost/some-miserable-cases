cb6a8e73140ca02d983364a0b503a5e7908ea7ce
dioz-alterpost <Linkin122223park>
2018-05-02 18:52:14 +0300

    second try

--- src/NestVar.java
+++ src/NestVar.java
@@ -4,7 +4,7 @@ public class NestVar {
 
         for(count = 0; count < 10; count = count + 1) {
             System.out.println("This is count: " + count);
-            int count;
+            //int count;
             for(count = 0; count < 2; count++)
                 System.out.println("This program is in error!");
         }
